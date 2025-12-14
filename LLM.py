8b16eef6ae31d3340e7272e02b582c449a864567
Umar-Farooq07 <umarabc2006@gmail.com>
2025-12-14 11:28:03 +0530

    added StoringRetrieval.py modified Lllm.py

--- LLM.py
+++ LLM.py
@@ -6,12 +6,7 @@ from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
 from langchain_core.prompts import PromptTemplate
 from langchain_core.messages import SystemMessage,AIMessage,HumanMessage
 
-hugginface_model = HuggingFaceEndpoint(
-    repo_id='deepseek-ai/DeepSeek-V3.2',
-    task = 'text-generation'
-    
-)
-hugginface_llm= ChatHuggingFace(llm=hugginface_model)
+
 
 
 
@@ -22,27 +17,32 @@ class QueryLLM():
                     Documentation: {context}
                     query: {query}
 
-                    use only the documentation provided and give the executable python code enclosed in ``` ```where ever necessary also do not invent any false endpoints or information on your own""",
+                    use only the documentation provided and give the executable python code enclosed in ``` ```when ever asked  also do not invent any false endpoints or information on your own""",
         input_variables=['query', 'context']
         )
     def __init__(self):
+        self.hugginface_model = HuggingFaceEndpoint(
+        repo_id='deepseek-ai/DeepSeek-V3.2',
+        task = 'text-generation'
         
-        chathistory = [
+        )
+        self.hugginface_llm= ChatHuggingFace(llm=self.hugginface_model)
+        
+        self.chathistory = [
             SystemMessage(content = """you are an expert software assistent.
                         follow the given instructions strictly do not give the answer without knowledge""")
         ]
     
         
     def query_llm(self, context,query):
-        self.chathistory.append(HumanMessage(content=self.template.invoke(context= context, query =query ).to_string()))
-        result = hugginface_llm.invoke(self.chathistory)
+        prompt_text= self.template.format(context=context,query= query)
+        self.chathistory.append(HumanMessage(content=prompt_text))
+        result = self.hugginface_llm.invoke(self.chathistory)
         self.chathistory.append(AIMessage(content=result.content))
         return result
 
 
-model = QueryLLM()
-response = model.query_llm("delhi is capital of india", 'what is capital of india')
-print(response.content)
+
 
 
 
---
+++ StoringRetrieval.py
@@ -0,0 +1,66 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+from chunking import MarkdownChunker
+from LLM import QueryLLM
+
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.messages import SystemMessage,AIMessage,HumanMessage
+
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name= "sentence-transformers/all-MiniLM-L6-v2"
+)
+
+
+
+class VectorStoreRetrival:
+
+    def __init__(self):
+        existing = os.listdir("vector_store")
+        new_id = len(existing) + 1  
+        self.db= Chroma(
+            collection_name= f"chat_{new_id}",
+            embedding_function=embedding_model,
+            persist_directory=f"vector_store/chat_{new_id}"
+        )
+        
+
+
+    def create_db(self,documents):
+        self.db.add_texts(documents)
+        self.db.persist()
+
+    def retrieve_data(self,query):
+        retriever = self.db.as_retriever(
+            search_type = "similarity",
+            search_kwargs={"k":3}
+        )
+        docs= retriever.invoke(query)
+        context = "\n\n".join([doc.page_content for doc in docs])
+        return context
+    
+
+
+
+Markdown_model= MarkdownChunker()
+Markdown_data = Markdown_model.convert_to_chunks("datum_doc.pdf")
+
+db_model= VectorStoreRetrival()
+db_model.create_db(Markdown_data)
+
+llm_model = QueryLLM()
+while(True):
+    query = input()
+    context= db_model.retrieve_data(query)
+    response = llm_model.query_llm(context,query)
+    print(response.content)
+
+
+
+
+
